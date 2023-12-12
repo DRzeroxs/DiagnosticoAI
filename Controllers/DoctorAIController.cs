d2854e82f51b694a5288ffdb28be440b82b4318c
DRzeroxs <mgrullonreinoso@gmail.com>
2023-12-12 03:12:21 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/DoctorAIController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DiagnosticoAI.Models;
+using RestSharp;
+using DiagnosticoAI.Recursos.ChatGPT;
+using Newtonsoft.Json;
+
+namespace DiagnosticoAI.Controllers
+{
+    public class DoctorAIController : Controller
+    {
+        public static string _EndPoint = "https://api.openai.com/";
+        public static string _URI = "v1/chat/completions";
+        public static string _APIKey = "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";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Paciente paciente)
+        {
+            string pSolicitud = "Hola, funcionas?";
+            var strRespuesta = "";
+
+            //Consumir api:
+            var oCliente = new RestClient(_EndPoint);
+            var oSolicitud = new RestRequest(_URI, Method.Post);
+
+            oSolicitud.AddHeader("Content-Type","application/json");
+            oSolicitud.AddHeader("Authorization", "Bearer "+_APIKey);
+
+            //Creamos el cuerpo de la solicitud
+
+            var oCuerpo = new Request()
+            {
+                model = "gpt-3.5-turbo-0613",
+                messages = new List<Message>()
+                {
+                    new Message()
+                    {
+                        role = "user",
+                        content = pSolicitud
+                    }
+                }
+            };
+
+            var jsonString = JsonConvert.SerializeObject(oCuerpo);
+
+            oSolicitud.AddJsonBody(jsonString);
+
+            //Generar la respuestas
+            //var oRespuesta = oCliente.Post<Response>(oSolicitud);
+
+            //strRespuesta = oRespuesta.choices[0].message.content;
+
+            ViewBag.Respuesta = strRespuesta;
+
+            return View("Conversar");
+        }
+
+        public ActionResult Conversar(string pSolicitud, string submitButton)
+        {
+            var strRespuesta = "";
+
+            if (submitButton == "Generar") {
+                //Consumir api:
+                var oCliente = new RestClient(_EndPoint);
+                var oSolicitud = new RestRequest(_URI, Method.Post);
+
+                oSolicitud.AddHeader("Content-Type", "application/json");
+                oSolicitud.AddHeader("Authorization", "Bearer " + _APIKey);
+
+                //Creamos el cuerpo de la solicitud
+
+                var oCuerpo = new Request()
+                {
+                    //-0613
+                    model = "gpt-3.5-turbo",
+                    messages = new List<Message>()
+                {
+                    new Message()
+                    {
+                        role = "user",
+                        content = pSolicitud
+                    }
+                }
+                };
+
+                var jsonString = JsonConvert.SerializeObject(oCuerpo);
+
+                oSolicitud.AddJsonBody(jsonString);
+
+                var oRespuesta = oCliente.Post<Response>(oSolicitud);
+
+                strRespuesta = oRespuesta.choices[0].message.content;
+
+                ViewBag.Respuesta = strRespuesta;
+            }
+            else if (submitButton == "Clear")
+            {
+                return View("Index", "DoctorAI");
+            }
+
+            
+
+            return View();
+        }
+    }
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,13 @@
+﻿namespace DiagnosticoAI.Models
+{
+    public class Paciente
+    {
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public string[] sintomas { get; set; }
+    }
+}
